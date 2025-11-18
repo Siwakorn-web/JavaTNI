a16ed03a9d07f67e0082d3b5d995b1d73214516e
Siwakorn-web <kengzelectric@gmail.com>
2025-11-18 13:59:53 +0700

    submit exercise Labsheet01

---
+++ Labsheet01/src/Lab101.java
@@ -0,0 +1,8 @@
+public class Lab101 {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Siwakorn Jitudomtham");
+        //System.out.println("\"INT-108\" \'Object-Oriented Programming\'");
+        System.out.println("This is my first time learning Java");
+        System.out.println("I hope to get grade \"A\"");
+    }
+}
---
+++ Labsheet01/src/Lab105.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class Lab105 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,
+                "Summation number = "+(2+5)+
+                        "\nSummation number = "+(2-5)+
+                        "\nMultiplication number = "+(2*5)+
+                        "\nDivision number = "+(2/5.0)+
+                        "\nModulus number = "+(2%5),
+                "Arithmetic Operators",
+
+                JOptionPane.WARNING_MESSAGE
+
+
+
+
+        );
+
+    }
+}
