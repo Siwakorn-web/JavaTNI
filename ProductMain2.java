947b816a91334a5d0345c1da42508239fb19d295
Siwakorn-web <kengzelectric@gmail.com>
2026-01-13 13:56:15 +0700

    Labsheet 07 Siwakorn

---
+++ ProductMain2.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+import  java.util.Scanner;
+
+public class ProductMain2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Product p1 = new Product();
+        p1.name = "Pens";
+        p1.price = 10.5;
+        p1.quantity = 50;
+
+        Product p2 = new Product();
+        p2.name = "Notebooks";
+        p2.price = 25.75;
+        p2.quantity = 20;
+
+        System.out.println("Welcome to Mini Shop" +
+                "---------------------------------" +
+                "Press [1] to buy Pen " +
+                "Press [2] to but Notebook");
+
+        System.out.print("Enter a number: ");
+        int press = sc.nextInt();
+        while (press!=1&&press!=2)
+        {
+            System.out.print("Invalaid number! Enter a number, again: ");
+            press= sc.nextInt();
+        }
+        if (press==1){
+
+            System.out.println();
+            p1.showInfo();
+            System.out.println("---------------------------------" );
+            System.out.print("How many Pens do you want to but ? ");
+            p1.sell(sc.nextInt());
+            System.out.println("---------------------------------" );
+            p1.showInfo();
+        }
+        else if (press==2)
+        {
+            System.out.println();
+            p2.showInfo();
+            System.out.println("---------------------------------" );
+            System.out.print("How many Pens do you want to but ? ");
+            p2.sell(sc.nextInt());
+            System.out.println("---------------------------------" );
+            p2.showInfo();
+        }
+
+    }
+}
